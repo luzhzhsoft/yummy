06f1f9e21654297758f4eab4091324cecb0a05b9
luzhzh <luzhzh@bw30.com>
2017-06-13 13:19:41 +0800

    web app

---
+++ src/main/java/com/luzhzh/yummy/App.java
@@ -0,0 +1,15 @@
+package com.luzhzh.yummy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by luzhzh on 6/13/2017.
+ */
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
