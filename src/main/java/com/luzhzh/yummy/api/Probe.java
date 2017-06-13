d17808d19b2164dde2f1834d3d376a6c2e13868c
luzhzh <luzhzh@bw30.com>
2017-06-13 13:42:53 +0800

    add probe

---
+++ src/main/java/com/luzhzh/yummy/api/Probe.java
@@ -0,0 +1,19 @@
+package com.luzhzh.yummy.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by luzhzh on 6/13/2017.
+ * 用来做k8s 存活检测
+ */
+@RestController
+public class Probe {
+
+    @RequestMapping("/liveness")
+    String getLivenessProbe() {
+        return "ok";
+    }
+
+
+}
