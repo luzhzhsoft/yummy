aedf143610efa4a8781fd4729a7c2e2b43aa10bf
luzhzh <luzhzh@bw30.com>
2017-06-14 09:13:45 +0800

    add redis

--- src/main/java/com/luzhzh/yummy/api/Hello.java
+++ src/main/java/com/luzhzh/yummy/api/Hello.java
@@ -1,7 +1,9 @@
 package com.luzhzh.yummy.api;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import redis.clients.jedis.Jedis;
 
 /**
  * Created by luzhzh on 6/13/2017.
@@ -13,4 +15,19 @@ public class Hello {
     String hello(){
         return "hello honey!";
     }
+
+    @RequestMapping("/hello/{name}")
+    String test(@PathVariable String name){
+        Jedis jedis=new Jedis("redis.yummy.svc");
+        jedis.set("name",name);
+
+        return "ok";
+    }
+
+    @RequestMapping("/get")
+    String get(){
+        Jedis jedis=new Jedis("redis.yummy.svc");
+        return jedis.get("name");
+    }
+
 }
